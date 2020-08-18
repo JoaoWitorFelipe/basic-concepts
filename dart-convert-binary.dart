b900c827814afc79d2c8f4d8bc7b5fb506fe01d4
Joao Witor Felipe <joaowitorfelipe2@gmail.com>
2020-08-18 20:51:45 -0300

    fix condition when calculate number

--- dart-convert-binary.dart
+++ dart-convert-binary.dart
@@ -1,4 +1,4 @@
-String convertDecimalToBinary(int n) => (n ~/ 2 > 2)
+String convertDecimalToBinary(int n) => (n ~/ 2 >= 2)
     ? generateBinary(convertDecimalToBinary(n ~/ 2), '${n % 2}')
     : generateBinary('${n ~/ 2}', '${n % 2}');
 
@@ -9,6 +9,5 @@ String reverseString(String text) =>
 
 void main() {
   //TODO: change functions names
-  //TODO: fix bug case pass parameter 10, 11...
-  print(reverseString(convertDecimalToBinary(25)));
+  print(reverseString(convertDecimalToBinary(3)));
 }
