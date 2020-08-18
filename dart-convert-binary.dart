a2b25e3cc7772802466d4fbd63923ac1fb3e4952
Joao Witor Felipe <joaowitorfelipe2@gmail.com>
2020-08-18 20:59:14 -0300

    rename functions

--- dart-convert-binary.dart
+++ dart-convert-binary.dart
@@ -1,13 +1,12 @@
 String convertDecimalToBinary(int n) => (n ~/ 2 >= 2)
-    ? generateBinary(convertDecimalToBinary(n ~/ 2), '${n % 2}')
-    : generateBinary('${n ~/ 2}', '${n % 2}');
+    ? buildBinaryInString(convertDecimalToBinary(n ~/ 2), '${n % 2}')
+    : buildBinaryInString('${n ~/ 2}', '${n % 2}');
 
-String generateBinary(String n, String preBinary) => '$preBinary' '$n';
+String buildBinaryInString(String n, String preBinary) => '$preBinary' '$n';
 
 String reverseString(String text) =>
     String.fromCharCodes(text.codeUnits.reversed);
 
 void main() {
-  //TODO: change functions names
-  print(reverseString(convertDecimalToBinary(3)));
+  print(reverseString(convertDecimalToBinary(4)));
 }
