575c7785ec140c2556ed99fd9194ea3481616c43
Joao Witor Felipe <joaowitorfelipe2@gmail.com>
2020-08-21 22:45:14 -0300

    add first algorithm in clojure

---
+++ clojure-operators-docs.clj
@@ -0,0 +1,15 @@
+(require '[clojure.repl :refer :all])
+
+
+(prn (+ 7 6 5 4))
+
+(prn (+ 1 2 3 4))
+
+(prn (/ (+ 7 (* 3 4) 5) 10.0))
+
+(doc rem)
+(doc mod)
+
+(find-doc "stack trace of the exception")
+
+
