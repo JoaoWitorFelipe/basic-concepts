881e1f8b55af86368183adbf45a6592bfa0735c1
Joao Witor Felipe <joaowitorfelipe2@gmail.com>
2020-08-23 11:58:35 -0300

    fix files name

--- dart-check-if-character-is-alphanumeric.dart
+++
@@ -1,8 +0,0 @@
-bool checkIfCharacterIsAlphaNumeric(any) => (any is String) || (any is int);
-
-void main() {
-  print(checkIfCharacterIsAlphaNumeric(""));
-  print(checkIfCharacterIsAlphaNumeric(5));
-  print(checkIfCharacterIsAlphaNumeric(2.6));
-  print(checkIfCharacterIsAlphaNumeric(true));
-}
