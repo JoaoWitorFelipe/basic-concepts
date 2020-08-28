a13df6f0c2c734785c0b9d17ba9e90aabd86d240
Joao Witor Felipe <joaowitorfelipe2@gmail.com>
2020-08-27 21:58:50 -0300

    add http function and anonymous functions

--- clojure_functions.clj
+++ clojure_functions.clj
@@ -50,11 +50,25 @@
 ;(triplicate2 #(prn "triplicate2"))
 
 ;10)
-(prn (Math/cos Math/PI))
-(defn math [x] (+ (Math/pow (Math/sin x) 2) (Math/pow (Math/cos x) 2)))
- 
-(prn (math 10))
+;(prn (Math/cos Math/PI))
+;(defn math [x] (+ (Math/pow (Math/sin x) 2) (Math/pow (Math/cos x) 2)))
+;(prn (math 10))
 
+;11)
+;(defn http-get [url]
+;    (slurp url)
+;  )
+;(prn (http-get "https://clojure.org/"))
+
+;12)
+;(defn one-less-arg [f x]
+;  (fn [& args] (apply f x args)))
+;(def one-less-arg-closure (one-less-arg + 1))
+;(prn (one-less-arg-closure 2))
+
+;13)
+;(defn two-fns [f g]
+;  (fn [x] (f (g x))))
 
 
 
